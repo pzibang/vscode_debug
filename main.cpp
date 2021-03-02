dd7ba9d8a40c04d0f12c602746187679e7ed2bdb
pengzibang <pengzibang@163.com>
2021-03-02 07:58:11 -0800

    状态模式

--- main.cpp
+++ main.cpp
@@ -4,16 +4,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+using namespace std;
+
                                                                                                       
-#include<sys/types.h>
-#include<regex.h>
-#include<memory.h>
+
+class State;
+class Work;
+class ForenoonState;
+class NoonState;
+
+class State
+{
+public:
+    virtual void WriteProgram(Work* w) = 0;
+
+};
+
+class Work
+{
+private:
+    State* current;
+public:
+    double hour;
+    Work();
+    ~Work(){}
+    void SetState(State* temp)
+    {
+        current = temp;
+    }
+
+    void Writeprogram()
+    {
+        current->WriteProgram(this);
+    }
+};
+
+class NoonState
+    : public State
+{
+
+public:
+    virtual void WriteProgram(Work* w)
+    {
+        cout<<"execute" << endl;
+        if((w->hour)<13)
+            cout<<"还不错啦" << endl;
+        else
+            cout<<"不行了，还是睡觉吧" << endl;       
+    }
+
+};
+
+class ForenoonState
+    : public State
+{
+public:
+    virtual void WriteProgram(Work* w)
+    {
+        if((w->hour)<12)
+            cout<<"现在的精神无敌好"<<endl;
+        else
+        {
+            w->SetState(new NoonState());
+            w->Writeprogram(); //注意加上这句
+        }
+    }
+
+};
 
 
+Work::Work()
+{
+    current = new ForenoonState();
+}
 
 int main()
 {
-
+    Work* mywork = new Work();
+    mywork->hour = 9;
+    mywork->Writeprogram();
+    mywork->hour = 14;
+    mywork->Writeprogram();    
     return 0;
 }
 
